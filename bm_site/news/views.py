37ac4164c9306dcfa53c4835fc3bf50606f8d1dd
Сунцов Владимир <vovka.steel911@mail.ru>
2020-06-26 13:02:39 +0300

    Вывод сообщения МЧС

--- bm_site/news/views.py
+++ bm_site/news/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.views.generic import TemplateView
-from .models import ExchangeRates
+from .models import ExchangeRates, EmergencyWarnings
 
 
 class NewsView(TemplateView):
@@ -9,8 +9,10 @@ class NewsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # test_date = datetime.strptime('19.06.2020', '%d.%m.%Y')
         context['data'] = {
             'euro': ExchangeRates.objects.filter(currency='EUR', rate_date__lte=datetime.now()).first(),
-            'dollar': ExchangeRates.objects.filter(currency='USD', rate_date__lte=datetime.now()).first()
+            'dollar': ExchangeRates.objects.filter(currency='USD', rate_date__lte=datetime.now()).first(),
+            'emergency': EmergencyWarnings.objects.filter(pub_date__date=datetime.now()),
         }
         return context['data']
