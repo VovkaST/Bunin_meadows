d16ac9269f93c29f24f6dbc10a4a69bd6340a704
Сунцов Владимир <vovka.steel911@mail.ru>
2020-06-28 20:19:07 +0300

    Изменены настройки Django: перешли на pathlib, static и templates переехали в главный каталог

--- bm_site/bm_site/settings/base.py
+++ bm_site/bm_site/settings/base.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pathlib
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'news',
 ]
 
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'bm_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +81,7 @@ WSGI_APPLICATION = 'bm_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -118,4 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+NEWSFEED_ROOT = STATIC_ROOT / 'newsfeed'
+STATICFILES_DIRS = [
+    ('css', STATIC_ROOT / 'css'),
+    ('fonts', STATIC_ROOT / 'fonts'),
+    ('images', STATIC_ROOT / 'images'),
+    ('newsfeed', NEWSFEED_ROOT),
+]
 STATIC_URL = '/static/'
